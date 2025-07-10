22336cf070bc24febb2e30558ac3fdffe3ceefc3
Minyong Ha <pol5412@naver.com>
2025-07-10 13:09:06 +0900

    2주차 django 개발 완료

---
+++ product/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import MainContent
+
+def index(request):
+    # -는 역순 정렬, 가장 최신 콘텐츠를 상단에 노출시키기 위함
+    content_list = MainContent.objects.order_by('-pub_date')
+    context = {'content_list' : content_list}
+    # render : content_list의 데이터를 product/content_list.html 파일에 적용 후 HTML을 리턴
+    return render(request, 'product/content_list.html', context)
